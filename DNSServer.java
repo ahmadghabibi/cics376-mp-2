a5415fd749cacfda1c2ee5d0fd24df91e6b4d2b7
ahmadhabibi-hope <ahmadhabibi877@yahoo.com>
2025-02-07 17:23:21 -0500

    add prnDNSHeader method

--- DNSServer.java
+++ DNSServer.java
@@ -8,63 +8,37 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.io.*;
 
+
+
 class DNSSrver
 {
     public static void prnName(DatagramPacket packet)
     {
-        byte bytes[] = packet.getData();
+        ByteBuffer dnsBuffer = ByteBuffer.wrap(packet.getData());
+        while(dnsBuffer.hasRemaining())
+        {
+            System.out.print(dnsBuffer.get() + " ");
+        }
         for (int i =0;i<packet.getLength();i++)
         {
-            System.out.print((char)bytes[i]);
+            //System.out.print((char)bytes[i]);
         }
         System.out.println("");
         System.out.println("-----------------------------------");
     }
-    public static int printDNSName(byte bytes[], int start)
-    {
-       
-        int pos = start;
-        while (bytes[pos] != 0) {
-            if (pos != start) 
-            {
-                System.out.print(".");
-            }
-              
-            int length = bytes[pos];
     
-            // POINTER!  We recursively print from a different place in the packet
-            if (length == -64) {
-                int pos2 = bytes[pos+1] & 0xFF;
-                printDNSName(bytes, pos2);
-                pos++;
-                break;
-    
-            // Otherwise the "length" is the number of characters in this part of
-            // name.
-            } else {
-                for (int i=1; i<=length; i++) {
-                    System.out.print((char)bytes[pos+i]);
-                }
-                pos += length+1;
-            }
-        }
-        pos ++;
-        return pos;
-    }
-    public static void  printDNSPacket(DatagramPacket packet) 
-    {
-       
-       int pos =  printDNSName(packet.getData(),packet.getLength());
-       System.out.println(pos);
-      
-       // System.out.println("*** packet received from " + packet.getAddress().getHostAddress());
-        //System.out.println("*** packet received from " + packet.getData());
-    }
     public static int extractTranId(byte[] dnsQuery)
     {
 
         ByteBuffer dnsBuffer = ByteBuffer.wrap(dnsQuery);
         int dnsQueryTranId  = dnsBuffer.getShort() & 0xFFFF;
+
+       // ByteBuffer dnsResponseBuffer = ByteBuffer.allocate(20);
+        //dnsResponseBuffer.putShort((short) dnsQueryTranId); // TransId
+
+       // System.out.println("Trans ID: " + String.format("%x",dnsQueryTranId));
+       // System.out.println("Trans ID in buffer " + dnsResponseBuffer.getShort());
+
         return dnsQueryTranId;
     }
 
@@ -85,6 +59,8 @@ class DNSSrver
         
        // Header section
        dnsResponseBuffer.putShort((short) transId); // TransId
+      // System.out.println((short) dnsResponseBuffer.getShort());
+
        dnsResponseBuffer.putShort((short) 0x8180); // Flags (standard query response, no error)
        dnsResponseBuffer.putShort((short) 1);      // Questions
        dnsResponseBuffer.putShort((short) 1);      // Answer RRs
@@ -154,7 +130,7 @@ class DNSSrver
     public static String extractDomainNames(byte[] dnsQueryPacket) throws IOException
     {
 
-        ByteBuffer dnsByteBuffer = ByteBuffer.wrap(dnsQueryPacket);
+        ByteBuffer dnsByteBuffer = ByteBuffer.wrap(dnsQueryPacket); // create byte byfferArray
         dnsByteBuffer.position(12); // domain name starts here at position 12
         StringBuilder hostName = new StringBuilder();
         int len;
@@ -175,6 +151,71 @@ class DNSSrver
         }
         return hostName.toString();
     }
+    public static String hexArrayToStr(byte[] hexArray)
+    {
+         StringBuilder sb = new StringBuilder();
+         for (byte b: hexArray)
+         {
+            sb.append((char) b);
+         }
+         return sb.toString();
+    }
+
+
+     
+    public static int printDNSName(byte bytes[], int start)
+    {
+       
+        int pos = start;
+        while (bytes[pos] != 0)
+         {
+            if (pos != start) 
+            {
+                System.out.print(".");
+            }
+              
+            int length = bytes[pos];
+    
+            // POINTER!  We recursively print from a different place in the packet
+            if (length == -64) {
+                int pos2 = bytes[pos+1] & 0xFF;
+                printDNSName(bytes, pos2);
+                pos++;
+                break;
+    
+            // Otherwise the "length" is the number of characters in this part of
+            // name.
+            } else {
+                for (int i=1; i<=length; i++) {
+                    System.out.print((char)bytes[pos+i]);
+                }
+             
+                pos += length+1;
+            }
+        }
+        pos ++;
+        System.out.println("");
+        return pos;
+    }
+    public static void  printDNSPacket(DatagramPacket packet) 
+    {
+
+       int domainNameStartPos = 12 ;
+       int pos =  printDNSName(packet.getData(),domainNameStartPos);
+      
+    }
+
+    public static void prnDNSHeader(byte[] query)
+    {
+
+        System.out.printf("Trans Id: 0x%02X%02X\n ",query[0],query[1]);
+        System.out.printf("Flags: 0x%02X%02X\n ",query[2],query[3]);
+        System.out.printf("Questions: %d\n",(query[4] << 8 ) | query[5]);
+        System.out.printf("Answer RRs: %d\n",(query[6] << 8 ) | query[7]);
+        System.out.printf("Authority RRs: %d\n",(query[8] << 8 ) | query[9]);
+        System.out.printf("Additional RRs: %d\n",(query[10] << 8 ) | query[11]);
+    }
+
     public static void main(String[] args) throws IOException
     {
     
@@ -189,8 +230,12 @@ class DNSSrver
        while (true) 
        {
            socket.receive(packet);  
+           //printDNSPacket(packet) ;
+           prnDNSHeader(packet.getData());
            InetAddress client_ip = packet.getAddress();
            int client_port = packet.getPort();
+
+
            String domainNames = extractDomainNames(packet.getData());  
            if (domainNames.matches(myDomainName))
            {
@@ -200,8 +245,13 @@ class DNSSrver
            {
                 dnsResponse =  DNSSrver.createNotFoundResponse(packet,myDomainName);
            }
+
            packet = new DatagramPacket(dnsResponse,dnsResponse.length, client_ip,client_port);        
            socket.send(packet);
+
+           System.out.println("---------------------------------------");
+           prnDNSHeader(dnsResponse);
+          
          }
     }
 }
